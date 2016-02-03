37fffbc97adf724c8930f84da660318af810207d
Augustine Onubeze <augustine.onubeze@metropolia.fi>
2016-02-03 17:29:56 +0200

    Remove options menu

--- VitalsensApp/app/src/main/java/vitalsens/vitalsensapp/activities/MainActivity.java
+++ VitalsensApp/app/src/main/java/vitalsens/vitalsensapp/activities/MainActivity.java
@@ -13,8 +13,6 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -144,29 +142,6 @@ public class MainActivity extends Activity {
 
     private static IntentFilter sensorStatusUpdateIntentFilter() {
         return new IntentFilter();
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     private void showMessage(final String msg) {
