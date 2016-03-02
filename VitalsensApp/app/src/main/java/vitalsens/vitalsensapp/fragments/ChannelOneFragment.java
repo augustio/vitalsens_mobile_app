eb6b4eecdd83e96a62ebf9af17e03d2e3f49e743
Augustine Onubeze <augustine.onubeze@metropolia.fi>
2016-03-02 16:49:50 +0200

    Set app to display sensor data in near-real time

--- VitalsensApp/app/src/main/java/vitalsens/vitalsensapp/fragments/ChannelOneFragment.java
+++ VitalsensApp/app/src/main/java/vitalsens/vitalsensapp/fragments/ChannelOneFragment.java
@@ -19,16 +19,12 @@ public class ChannelOneFragment extends Fragment {
 
     private static final int MIN_Y = 0;//Minimum ECG data value
     private static final int MAX_Y = 4095;//Maximum ECG data value
-    private static final int X_RANGE = 200;
+    private static final int X_RANGE = 500;
 
     private LineGraphView mLineGraph;
     private GraphicalView mGraphView;
 
-    private Handler mHandler;
-
     private int xValueCounter;
-    private boolean graphStarted;
-    private ArrayList<Integer> mCollection;
 
     public ChannelOneFragment() {
     }
@@ -38,10 +34,6 @@ public class ChannelOneFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         xValueCounter = 0;
-        mHandler = new Handler();
-        graphStarted = false;
-        mCollection = new ArrayList<>();
-
     }
 
     @Override
@@ -56,35 +48,20 @@ public class ChannelOneFragment extends Fragment {
     }
 
     public void updateGraph(int value) {
-        if(!graphStarted) {
-            graphStarted = true;
-            mStartGraph.run();
+        if(mGraphView != null) {
+            double maxX = xValueCounter;
+            double minX = (maxX < X_RANGE) ? 0 : (maxX - X_RANGE);
+            mLineGraph.setXRange(minX, maxX);
+            mLineGraph.addValue(new Point(xValueCounter, value));
+            xValueCounter++;
+            mGraphView.repaint();
         }
-        mCollection.add(value);
     }
-
-    private final Runnable mStartGraph = new Runnable() {
-        @Override
-        public void run() {
-            if(xValueCounter < mCollection.size()) {
-                double maxX = xValueCounter;
-                double minX = (maxX < X_RANGE) ? 0 : (maxX - X_RANGE);
-                mLineGraph.setXRange(minX, maxX);
-                mLineGraph.addValue(new Point(xValueCounter, mCollection.get(xValueCounter)));
-                xValueCounter++;
-                mGraphView.repaint();
-            }
-            mHandler.postDelayed(mStartGraph, 1);
-        }
-    };
 
     public void clearGraph(){
         if(mGraphView != null) {
             mGraphView.repaint();
             mLineGraph.clearGraph();
-            graphStarted = false;
-            mCollection.clear();
-            mHandler.removeCallbacks(mStartGraph);
             xValueCounter = 0;
         }
     }
